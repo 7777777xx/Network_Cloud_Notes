41326df2fded0036a3aa5bce3c2f7a834b69bf05
7xx <1054601634@@qq.com>
2021-01-09 16:39:21 +0800

    添加缓存及分页功能

--- net_note/note/urls.py
+++ net_note/note/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from note import views
 
 urlpatterns = [
-    path('', views.list_view),
+    path('', views.list_view, name='list_note'),
     path('add', views.add_view),
     path('add', views.add_view),
     path('mod/<int:id>', views.mod_view),
--- net_note/note/views.py
+++ net_note/note/views.py
@@ -1,16 +1,18 @@
 import html
 
+from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from .models import Note
 from tools.login_check import login_check
+
 
 # Create your views here.
 
 
-
-
 # 显示笔记列表
+@cache_page(10)
 @login_check
 def list_view(request):
     uname = request.session['uname']
@@ -18,12 +20,15 @@ def list_view(request):
     print(uid)
     try:
         notes = Note.objects.filter(user_id=uid)
+        paginator = Paginator(notes, 1)
+        cur_page = request.GET.get('page', 1)  # 得到默认的当前页
+        page = paginator.page(cur_page)
         print('11111 %s' % notes)
     except Exception as e:
         print('error is %s' % e)
 
-
     return render(request, 'note/list_note.html', locals())
+
 
 # 添加笔记
 @login_check
@@ -40,6 +45,7 @@ def add_view(request):
         uid = request.session['uid']
         Note.objects.create(title=title, content=content, user_id=uid)
         return HttpResponseRedirect('/note/')
+
 
 # 修改笔记
 @login_check
@@ -60,7 +66,6 @@ def mod_view(request, id):
         return HttpResponseRedirect('/note/')
 
 
-
 # 删除笔记
 @login_check
 def del_view(request, id):
@@ -71,6 +76,3 @@ def del_view(request, id):
         print('error is %s ' % e)
         return HttpResponse('未找到该笔记!')
     return HttpResponseRedirect('/note/')
-
-
-
